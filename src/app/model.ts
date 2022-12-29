b46bb93ce72ccfbd3d10117b39d0f67e1b72c499
kendrick chukwuka <kendrickchibueze@gmail.com>
2022-12-29 18:46:40 +0100

    initial commit

---
+++ src/app/model.ts
@@ -0,0 +1,33 @@
+export class Model {
+   user;
+   items
+
+
+   constructor(){
+     this.user="kendrick";
+     this.items=[new TodoItem("Buy Flowers", false),
+                new TodoItem("Get Shoes", false),
+                new TodoItem("Collect Tickets", true),
+                new TodoItem("Call Joe", false)]
+    }
+}
+
+export class TodoItem {
+      action;
+      done
+
+      constructor(action, done){
+         this.action = action,
+         this.done = done
+      }
+}
+
+
+
+
+
+
+
+
+
+
